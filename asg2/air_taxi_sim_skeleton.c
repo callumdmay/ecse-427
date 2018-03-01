83acda160ec752c8c062e8a0493a72a4706972dc
callumdmay <callumdmay@gmail.com>
2018-03-01 10:20:06 -0500

    Fix indentation/formatting, add thread creation

--- asg2/air_taxi_sim_skeleton.c
+++ asg2/air_taxi_sim_skeleton.c
@@ -13,7 +13,7 @@
 //Name: Callum May
 //McGill ID: 260564523
 
- 
+
 
 #include <stdio.h>
 #include <pthread.h>
@@ -28,44 +28,39 @@ int BUFFER_SIZE = 100; //size of queue
 
 
 // A structure to represent a queue
-struct Queue
-{
+struct Queue {
     int front, rear, size;
     unsigned capacity;
-    int* array;
+    int *array;
 };
- 
+
 // function to create a queue of given capacity. 
 // It initializes size of queue as 0
-struct Queue* createQueue(unsigned capacity)
-{
-    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+struct Queue *createQueue(unsigned capacity) {
+    struct Queue *queue = (struct Queue *) malloc(sizeof(struct Queue));
     queue->capacity = capacity;
     queue->front = queue->size = 0;
     queue->rear = capacity - 1;  // This is important, see the enqueue
-    queue->array = (int*) malloc(queue->capacity * sizeof(int));
+    queue->array = (int *) malloc(queue->capacity * sizeof(int));
     return queue;
 }
- 
+
 // Queue is full when size becomes equal to the capacity 
-int isFull(struct Queue* queue)
-{
-    return ((queue->size ) >= queue->capacity);
+int isFull(struct Queue *queue) {
+    return ((queue->size) >= queue->capacity);
 }
- 
+
 // Queue is empty when size is 0
-int isEmpty(struct Queue* queue)
-{
+int isEmpty(struct Queue *queue) {
     return (queue->size == 0);
 }
- 
+
 // Function to add an item to the queue.  
 // It changes rear and size
-void enqueue(struct Queue* queue, int item)
-{
+void enqueue(struct Queue *queue, int item) {
     if (isFull(queue))
         return;
-    queue->rear = (queue->rear + 1)%queue->capacity;
+    queue->rear = (queue->rear + 1) % queue->capacity;
     queue->array[queue->rear] = item;
     queue->size = queue->size + 1;
     printf("%d enqueued to queue\n", item);
@@ -73,81 +68,116 @@ void enqueue(struct Queue* queue, int item)
 
 // Function to remove an item from queue. 
 // It changes front and size
-int dequeue(struct Queue* queue)
-{
+int dequeue(struct Queue *queue) {
     if (isEmpty(queue))
         return INT_MIN;
     int item = queue->array[queue->front];
-    queue->front = (queue->front + 1)%queue->capacity;
+    queue->front = (queue->front + 1) % queue->capacity;
     queue->size = queue->size - 1;
     return item;
 }
 
 // Function to get front of queue
-int front(struct Queue* queue)
-{
+int front(struct Queue *queue) {
     if (isEmpty(queue))
         return INT_MIN;
     return queue->array[queue->front];
 }
- 
+
 // Function to get rear of queue
-int rear(struct Queue* queue)
-{
+int rear(struct Queue *queue) {
     if (isEmpty(queue))
         return INT_MIN;
     return queue->array[queue->rear];
 }
 
-void print(struct Queue* queue){
-    if (queue->size == 0){
+void print(struct Queue *queue) {
+    if (queue->size == 0) {
         return;
     }
-    
-    for (int i = queue->front; i < queue->front +queue->size; i++){
-        
-        printf(" Element at position %d is %d \n ", i % (queue->capacity ), queue->array[i % (queue->capacity)]);
+
+    for (int i = queue->front; i < queue->front + queue->size; i++) {
+        printf(" Element at position %d is %d \n ", i % (queue->capacity), queue->array[i % (queue->capacity)]);
     }
-    
+
 }
 
-struct Queue* queue;
+struct Queue *queue;
 
 /*Producer Function: Simulates an Airplane arriving and dumping 5-10 passengers to the taxi platform */
-void *FnAirplane(void* cl_id)
-{
+void *FnAirplane(void *cl_id) {
+    printf("In thread");
+    int airplane_id = *((int *) cl_id);
+    free(cl_id);
+    printf("Created airplane with id %d\n", airplane_id);
 }
 
 /* Consumer Function: simulates a taxi that takes n time to take a passenger home and come back to the airport */
-void *FnTaxi(void* pr_id)
-{
+void *FnTaxi(void *pr_id) {
+    int taxi_id = *((int *) pr_id);
+    free(pr_id);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-  int num_airplanes;
-  int num_taxis;
+    int num_airplanes;
+    int num_taxis;
 
-  num_airplanes=atoi(argv[1]);
-  num_taxis=atoi(argv[2]);
-  
-  printf("You entered: %d airplanes per hour\n",num_airplanes);
-  printf("You entered: %d taxis\n", num_taxis);
-  
-  
-  //initialize queue
-  queue = createQueue(BUFFER_SIZE);
-  
-  //declare arrays of threads and initialize semaphore(s)
+    num_airplanes = atoi(argv[1]);
+    num_taxis = atoi(argv[2]);
 
-  //create arrays of integer pointers to ids for taxi / airplane threads
-  int *taxi_ids[num_taxis];
-  int *airplane_ids[num_airplanes];
-    
-  //create threads for airplanes
+    printf("You entered: %d airplanes per hour\n", num_airplanes);
+    printf("You entered: %d taxis\n", num_taxis);
 
-  //create threads for taxis
-  
-  pthread_exit(NULL);
+
+    //initialize queue
+    queue = createQueue(BUFFER_SIZE);
+
+    //declare arrays of threads and initialize semaphore(s)
+    pthread_t *airplane_threads = malloc(sizeof(pthread_t) * num_airplanes);
+    pthread_t *taxi_threads = malloc(sizeof(pthread_t) * num_taxis);
+
+    //create arrays of integer pointers to ids for taxi / airplane threads
+    int *taxi_ids[num_taxis];
+    int *airplane_ids[num_airplanes];
+
+    int i = 0, ret = -1;
+    //create threads for airplanes
+
+    for (i = 0; i < num_airplanes; i++) {
+        printf("Creating airplane thread %d\n", i);
+
+        //Allocate the memory for thread argument
+        int *arg = malloc(sizeof(*arg));
+        if (arg == NULL) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+        //Set the thread argument to an integer
+        *arg = i;
+        ret = pthread_create(&airplane_threads[i], NULL, &FnAirplane, i);
+        if (ret != 0) {
+            printf("Create pthread error!\n");
+            exit(1);
+        }
+    }
+
+    //create threads for taxis
+    for (i = 0; i < num_taxis; i++) {
+        printf("Creating taxi thread %d\n", i);
+        //Allocate the memory for thread argument
+        int *arg = malloc(sizeof(*arg));
+        if (arg == NULL) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+        //Set the thread argument to an integer
+        *arg = i;
+        ret = pthread_create(&airplane_threads[i], NULL, &FnTaxi, i);
+        if (ret != 0) {
+            printf("Create pthread error!\n");
+            exit(1);
+        }
+    }
+    pthread_exit(NULL);
 }
